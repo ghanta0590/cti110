baa504ea0d587e42ec29898eccf7e6f992dcce2b
ghanta0590 <113908728+ghanta0590@users.noreply.github.com>
2022-09-26 09:32:07 -0400

    Add files via upload

---
+++ P1HW2_GhantAmber.py
@@ -0,0 +1,28 @@
+print('This program calculates and displays expenses for a family trip.')
+print('September 26, 2022')
+print('CTI-110 P1HW2 - Travel Expense')
+print('Amber Ghant')
+print()
+print()
+budget = 750
+print('Enter Budget:', budget)
+destination = Carowinds
+print('Enter your travel destination:', destination)
+gas = 50
+print('How much do you think you will spend on gas?', gas)
+hotel = 300
+print('About how much will you need for a hotel?', 300)
+admission = 275
+print('Approximately how much will you need for park admission/parking?', admission)
+food = 100
+print ('How much do you plan to spend on food?', food)
+print('-----Travel Expenses-----')
+print('Location: Carowinds')
+print('Initial Budget: 750')
+print()
+print('Fuel: 50.0')
+print('Hotel: 300.0')
+print('Admission: 275.0')
+print('Food: 100.0')
+print()
+print('Remaining Balance: 25.0')
